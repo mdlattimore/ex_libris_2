626cb6062e9a47b662d72add403205a317473711
Mark D. Lattimore <mark.d.lattimore@gmail.com>
2026-01-16 22:37:40 -0500

    finshed first iteration of dashboard

--- pages/views.py
+++ pages/views.py
@@ -16,7 +16,7 @@
 
 from django.views.generic import TemplateView
 from .models import SiteContent
-
+import random
 
 class SiteContentMixin:
     """Provides the single SiteContent instance to templates."""
@@ -27,18 +27,30 @@ class SiteContentMixin:
 
 
 from django.views.generic import TemplateView
-from catalog.models import Volume, Work, Author, BookSet
+from catalog.models import Volume, Work, Author, BookSet, VolumeImage
 
 
 class DashboardView(TemplateView):
     template_name = "pages/dashboard.html"
 
     def get_context_data(self, **kwargs):
+        all_volumes = Volume.objects.all()
+        num_volumes = all_volumes.count()
+        vol_idx = random.randint(0, num_volumes - 1)
+        rand_vol = all_volumes[vol_idx]
+        cover = rand_vol.cover_src
+        blurb = rand_vol.description
+        rand_title = rand_vol.title
+
         context = super(DashboardView, self).get_context_data(**kwargs)
-        context["volumes"] = Volume.objects.all()
+        context["volumes"] = all_volumes
         context["works"] = Work.objects.all()
         context["authors"] = Author.objects.all()
         context["sets"] = BookSet.objects.all()
+        context["cover"] = cover
+        context["blurb"] = blurb
+        context["title"] = rand_title
+        context["rand_vol"] = rand_vol
 
 
         return context
